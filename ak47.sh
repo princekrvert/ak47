1ecde01086432f29c862e501309c655c8eb2614c
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2022-04-15 17:34:01 +0530

    Update ak47.sh
    version 1.0.1

--- ak47.sh
+++ ak47.sh
@@ -57,7 +57,7 @@ exitout(){
 updateme(){
     echo -e "\e[31;1m Checking version of the tool ...."
     version=$(curl -s https://raw.githubusercontent.com/princekrvert/ak47/main/version)
-    if [[ $version=="1.0.0" ]];then
+    if [[ $version=="1.0.1" ]];then
     echo "Tool is already updated.."
     else
     echo "\e[92;1m Updating ak47 "
